2039e859e72c7ed150c0f9def3992d37ff386415
Tatsiana Sapun <t.sapun@pixelplex.io>
2023-01-15 00:10:22 +0100

    feat: add app implementation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+import http from 'http';
+import { App } from './app';
+
+const server = http.createServer(App.listenRequest);
+
+const port = process.env.PORT;
+
+server.listen(port, () => console.log(`Server is running at ${port} port`));
